279d1670e3b549ef43e29efa33add5ef29371bab
mehdi-khoshkar <khoshkar.mehdi@gmail.com>
2019-07-10 11:25:24 +0430

    pop up

--- popup/PopUp.php
+++ popup/PopUp.php
@@ -58,48 +58,192 @@ add_action('wp_enqueue_scripts', 'popup_enqueue_script');
 
 
 
+function my_popup() {
+    add_meta_box( 'popup_meta_box',
+        'تنظیمات پاپ آپ',
+        'display_popup_meta_box',
+        'popup', 'normal', 'high'
+    );
+}
+
+add_action( 'admin_init', 'my_popup' );
+
+
+
+
+
+function display_popup_meta_box( $popup ) {
+
+   
+    $type_popup = esc_html( get_post_meta( $popup->ID, 'type_popup ', true ) );
+
+    $meta = get_post_meta(get_the_ID(), 'type_popup', true);
+
+    if($meta == 'all'){
+        $checked_all ='checked';
+    }
+    if($meta == 'all_post'){
+        $checked_post ='checked';
+    }
+    if($meta == 'all_page'){
+        $checked_page ='checked';
+    }
+    
+$html = '<input type="radio" id="type_popup" name="type_popup" value="all" '.$checked_all .'/>';
+    $html .= '<label for="type_popup">تمام صفحات</label>';
+ 
+    echo $html;
+
+    $html = '<input type="radio" id="type_popup" name="type_popup" value="all_post" '.$checked_post .'/>';
+    $html .= '<label for="type_popup"> تعبیر خواب</label>';
+    echo $html;
+
+    
+    $html = '<input type="radio" id="type_popup" name="type_popup" value="all_page" '.$checked_page .'/>';
+    $html .= '<label for="type_popup"> برگه ها</label>';
+    echo $html;
+  
+   
+}
+
+
+function add_popup_fields( $popup_id, $popup) {
+    // Check post type for channel
+   
+    if ( $popup->post_type == 'popup' ) {
+        // Store data in post meta table if present in post data
+      
+        if ( isset( $_POST['type_popup'] ) && $_POST['type_popup'] != '' ) {
+            update_post_meta( $popup_id, 'type_popup', $_POST['type_popup'] );
+           
+        }
+    }
+  
+    
+}
+
+
+add_action( 'save_post', 'add_popup_fields', 10, 2 );
+
+
+
+
+
+
+
 
 
 add_action( 'wp_head', 'my_header_scripts' );
 function my_header_scripts(){
 
-    $my_postid = 2785;//This is page id or post id
-    $content_post = get_post($my_postid);
+    global $wpdb;
+    $table_name = $wpdb->prefix.'postmeta';
+    $result = $wpdb->get_results ( "
+    SELECT * FROM $table_name WHERE meta_key = 'type_popup'
+    " );
+   
+    set_valid($result);
+?>
+    <script>   
+ 
+ function set_cookie(website) {
+
+var cookie_name = website;
+ if (-1 === document.cookie.indexOf(cookie_name)) {
+
+     var content_popup = $( "#popup_container" ).html();
+     setTimeout(function(){ 
+ Swal.fire({
+
+ html:content_popup,
+ showCloseButton: true,
+ showCancelButton: false,
+ focusConfirm: false,
+ confirmButtonText:
+ 'ممنون',
+
+ })
+ }, 3000);
+ document.cookie = cookie_name +'=returning_'+cookie_name+';max-age=86400;path=/;'
+
+
+ }
+ }
+ </script>
+
+ <?php
+
+
+}
+    
+
+
+function set_valid($result){
+
+      
+    global $wpdb;
+    $table_name = $wpdb->prefix.'postmeta';
+    $result = $wpdb->get_results ( "SELECT * FROM $table_name WHERE meta_key = 'type_popup' " );
+
+        foreach ( $result as $popup ){
+ 
+
+               if($popup->meta_value == 'all')
+                    {
+                    
+                    if(!is_front_page() && !is_singular('dream') && !isset($_COOKIE['asaay']) ){ 
+                    content_popup($popup->post_id)
+                    ?>
+                        <script>  
+            
+                    jQuery( document ).ready(function() {   
+                                set_cookie('asaay'); 
+                            });
+                                </script>
+            
+                <?php   }  }
+
+                if($popup->meta_value == 'all_post')
+                {
+
+                if(!is_front_page()  && is_singular('dream') && !isset($_COOKIE['asaay_post']) ){ 
+                  content_popup($popup->post_id) ?>
+                    <script>  
+
+                    jQuery( document ).ready(function() {   
+                                set_cookie('asaay_post'); 
+                            });
+                    </script>
+                <?php  } }
+                    if($popup->meta_value == 'all_page')
+                    {
+
+                    if(!is_front_page() && is_page() && !isset($_COOKIE['asaay_page']) ){ 
+
+                  content_popup($popup->post_id) ?>
+                        <script>  
+
+                        jQuery( document ).ready(function() {   
+                                    set_cookie('asaay_page'); 
+                                });
+                        </script>
+
+                    <?php } }
+
+
+        }
+
+
+      
+    
+}
+
+
+function content_popup($popup_id){
+
+    $content_post = get_post($popup_id);
     $content = $content_post->post_content;
-    $content = apply_filters('the_content', '<div id="popup_container" style="display:none;">'.$content.'</div>');
-    echo $content;
-
-    if(!is_front_page() && !isset($_COOKIE['asheghnameh'])){
-  ?>
-  <script>
-  
-  jQuery( document ).ready(function() {
-    if (-1 === document.cookie.indexOf('returning_asheghnameh')) {
-
-        var content_popup = $( "#popup_container" ).html();
-  Swal.fire({
-
-  html:content_popup,
-  showCloseButton: true,
-  showCancelButton: false,
-  focusConfirm: false,
-  confirmButtonText:
-    'ممنون',
-
-})
-document.cookie = 'asheghnameh=returning_asheghnameh;  max-age=86400;path=/;'
-
-
-    }
-
-});
-  
-</script>
-
-
-  <?php
+    $content = apply_filters('the_content', '<div id="popup_container"style="display:none">'.$content.'</div>');
+    echo $content;    
 }
-}
-
-
 
