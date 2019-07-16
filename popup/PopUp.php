8e765aaeafffe6f63da9aeacbd5c04ff99f7ec21
mehdi-khoshkar <khoshkar.mehdi@gmail.com>
2019-07-16 17:48:14 +0430

    Update popup

--- popup/PopUp.php
+++ popup/PopUp.php
@@ -78,32 +78,26 @@ function display_popup_meta_box( $popup ) {
     $type_popup = esc_html( get_post_meta( $popup->ID, 'type_popup ', true ) );
 
     $meta = get_post_meta(get_the_ID(), 'type_popup', true);
+	    $meta_special = get_post_meta(get_the_ID(), 'type_popup_special', true);
 
-    if($meta == 'all'){
-        $checked_all ='checked';
-    }
-    if($meta == 'all_post'){
-        $checked_post ='checked';
-    }
-    if($meta == 'all_page'){
-        $checked_page ='checked';
-    }
-    
-$html = '<input type="radio" id="type_popup" name="type_popup" value="all" '.$checked_all .'/>';
-    $html .= '<label for="type_popup">تمام صفحات</label>';
- 
-    echo $html;
+echo $meta;
+	echo  $meta_special;
 
-    $html = '<input type="radio" id="type_popup" name="type_popup" value="all_post" '.$checked_post .'/>';
-    $html .= '<label for="type_popup"> تعبیر خواب</label>';
-    echo $html;
+	  $categories = get_categories();
+	
 
-    
-    $html = '<input type="radio" id="type_popup" name="type_popup" value="all_page" '.$checked_page .'/>';
-    $html .= '<label for="type_popup"> برگه ها</label>';
-    echo $html;
-  
-   
+
+			foreach($categories as $category) { ?>
+
+<input type="radio" id="type_popup" name="type_popup"  value="<?php echo $category->term_id ?>" <?php if($category->term_id == $meta && empty($_GET['type_popup_special'])){ echo 'checked';}?>/>
+				<label for="type_popup"> <?php echo $category->name; ?></label>
+<?php
+			}
+	?>
+		<input type="text" id="type_popup_special" name="type_popup_special"  value="<?php if(empty($_GET['type_popup'])){echo $meta_special;}?>"/>
+				<label for="type_popup"> شناسه پست</label>
+
+<?php
 }
 
 
@@ -113,8 +107,15 @@ function add_popup_fields( $popup_id, $popup) {
     if ( $popup->post_type == 'popup' ) {
         // Store data in post meta table if present in post data
       
-        if ( isset( $_POST['type_popup'] ) && $_POST['type_popup'] != '' ) {
+        if ( isset( $_POST['type_popup'] ) && $_POST['type_popup'] != '' && $_POST['type_popup_special'] == '' ) {
+			  delete_post_meta($popup_id, 'type_popup_special', $_POST['type_popup_special']);
             update_post_meta( $popup_id, 'type_popup', $_POST['type_popup'] );
+           
+        }
+		
+		  if ( isset( $_POST['type_popup_special'] ) && $_POST['type_popup_special'] != '' ) {
+			  delete_post_meta($popup_id, 'type_popup', $_POST['type_popup']);
+            update_post_meta( $popup_id, 'type_popup_special', $_POST['type_popup_special'] );
            
         }
     }
@@ -136,13 +137,8 @@ add_action( 'save_post', 'add_popup_fields', 10, 2 );
 add_action( 'wp_head', 'my_header_scripts' );
 function my_header_scripts(){
 
-    global $wpdb;
-    $table_name = $wpdb->prefix.'postmeta';
-    $result = $wpdb->get_results ( "
-    SELECT * FROM $table_name WHERE meta_key = 'type_popup'
-    " );
-   
-    set_valid($result);
+	
+  set_valid();
 ?>
     <script>   
  
@@ -178,59 +174,40 @@ var cookie_name = website;
     
 
 
-function set_valid($result){
+function set_valid(){
 
       
     global $wpdb;
     $table_name = $wpdb->prefix.'postmeta';
     $result = $wpdb->get_results ( "SELECT * FROM $table_name WHERE meta_key = 'type_popup' " );
 
+	
         foreach ( $result as $popup ){
- 
-
-               if($popup->meta_value == 'all')
-                    {
-                    
-                    if(!is_front_page() && !is_singular('dream') && !isset($_COOKIE['asaay']) ){ 
+                $popup_categories = get_the_terms( $popup->ID, 'category' );
+               
+			foreach($popup_categories as $popup_category){
+				
+				if($popup->meta_value == $popup_category->term_id && get_post_status( $popup->post_id) == 'publish' ){
+			      $cookie_name = 'farmooon_'.$popup->meta_value;
+					echo '<span id="cookie_name" style=" display: none;">'.$cookie_name.'</span>';
+                    if(!is_front_page()  && $popup_category &&!isset($_COOKIE[$cookie_name]) ){ 
                     content_popup($popup->post_id)
                     ?>
                         <script>  
             
-                    jQuery( document ).ready(function() {   
-                                set_cookie('asaay'); 
+                    jQuery( document ).ready(function() { 
+						var cookie_name = $('#cookie_name').text();
+                                set_cookie(cookie_name); 
                             });
                                 </script>
             
-                <?php   }  }
+                <?php   } 
+			   
 
-                if($popup->meta_value == 'all_post')
-                {
+             
 
-                if(!is_front_page()  && is_singular('dream') && !isset($_COOKIE['asaay_post']) ){ 
-                  content_popup($popup->post_id) ?>
-                    <script>  
-
-                    jQuery( document ).ready(function() {   
-                                set_cookie('asaay_post'); 
-                            });
-                    </script>
-                <?php  } }
-                    if($popup->meta_value == 'all_page')
-                    {
-
-                    if(!is_front_page() && is_page() && !isset($_COOKIE['asaay_page']) ){ 
-
-                  content_popup($popup->post_id) ?>
-                        <script>  
-
-                        jQuery( document ).ready(function() {   
-                                    set_cookie('asaay_page'); 
-                                });
-                        </script>
-
-                    <?php } }
-
-
+}
+			}   
         }
 
 
@@ -247,3 +224,124 @@ function content_popup($popup_id){
     echo $content;    
 }
 
+
+
+
+function popup_shordcode($atts, $content = null){
+
+      
+global $post;
+	extract( shortcode_atts( array(
+      'post_id' => $post->post_id,
+      ), $atts ) );
+	
+     global $wpdb;
+    $table_name = $wpdb->prefix.'postmeta';
+    $result = $wpdb->get_results ( "SELECT * FROM $table_name WHERE meta_key = 'type_popup' " );
+	
+  
+
+        foreach ( $result as $popup ){
+
+			 
+               if($popup->meta_value == $post_id)
+                    {
+                    $cookie_name = 'farmooon_'.$post->post_id;
+					echo '<span id="cookie_name" style=" display: none;">'.$cookie_name.'</span>';
+                    if(!is_front_page() && is_single($post_id) && !isset($_COOKIE[$cookie_name]) ){ 
+                    content_popup($popup->post_id)
+                    ?>
+                        <script>  
+            var cookie_name = $('#cookie_name').text();
+                    jQuery( document ).ready(function() {   
+                                set_cookie(cookie_name); 
+                            });
+                                </script>
+            
+                <?php  
+					} 
+			   } 
+			  } 
+		
+	
+    
+}
+add_shortcode('popup', 'popup_shordcode');
+
+
+
+
+add_filter( 'manage_edit-popup_columns', 'my_edit_popup_columns' ) ;
+
+function my_edit_popup_columns( $columns ) {
+
+	$columns = array(
+		'cb' => '&lt;input type="checkbox" />',
+		'title' => __( 'پاپ آپ' ),
+		'short_code' => __( 'کد کوتاه' ),
+         'show' => __( 'نمایش در دسته بندی' ),
+		'active' => __( 'وضعیت' ),
+		 'date' => __( 'Date' )
+	);
+
+	return $columns;
+}
+
+
+add_action( 'manage_popup_posts_custom_column', 'my_manage_popup_columns', 10, 2 );
+
+function my_manage_popup_columns( $column, $post_id ) {
+	global $post;
+
+	switch( $column ) {
+
+		/* If displaying the 'short_code' column. */
+		case 'short_code' :
+	    $meta_special = get_post_meta(get_the_ID(), 'type_popup_special', true);
+
+			if($meta_special && get_post_status( $post->post_id ) == 'publish' ){
+						  echo '<input type="text" value="[popup post_id= '.$meta_special.'][/popup]" readonly style="width: 30%;">';
+
+			} else {
+				echo '-';
+			}
+
+			break;
+			
+			
+				/* If displaying the 'active' column. */
+		case 'active' :
+
+				 if (get_post_status( $post->post_id ) == 'publish' ) {
+			
+						  echo 'فعال';
+
+			} else {
+				echo 'غیرفعال';
+			}
+		
+		
+			break;
+			
+		
+	/* If displaying the 'show' column. */
+		case 'show' :
+	    $meta = get_post_meta(get_the_ID(), 'type_popup', true);
+
+			if($meta && get_post_status( $post->post_id ) == 'publish' ){
+				$cat_name = get_cat_name($meta );
+
+						  echo $cat_name;
+
+			} else {
+				echo '-';
+			}
+
+			break;
+
+
+		/* Just break out of the switch statement for everything else. */
+		default :
+			break;
+	}
+}
